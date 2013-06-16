dfc4cd025e698c778ed662a35bb00e2d757cb2fe
Georg Pelz <hybrid@selur.de>
2013-06-16 08:11:45 +0000

    fixed
    git-svn-id: https://selur.no-ip.info/svn/mkvCutter/trunk@66 7073fc58-22ce-48a9-8caa-da8ca8fc3f2d

--- mkvcutter.cpp
+++ mkvcutter.cpp
@@ -588,9 +588,12 @@ void MkvCutter::buildTrimAndPartsList()
     cutLength = cutEnd - cutStart;
     prevKey = cut.prevKey;
     nextKey = cut.nextKey;
+    this->addInfo(tr("  cutStart: %1, cutEnd: %2, prevKey: %3, nextKey: %4").arg(cutStart).arg(cutEnd).arg(prevKey).arg(nextKey));
+    this->addInfo(tr("  lastStartKey: %1, lastNextKey: %2, fileStartKey: %3, fileEndKey: %4").arg(lastStartKey).arg(lastNextKey).arg(fileStartKey).arg(fileEndKey));
+
     if (cutStart == prevKey && cutEnd == nextKey - 1) {
       fileIndex++;
-      this->addInfo("  " + tr("!append -> fileIndex %1").arg(fileIndex));
+      this->addInfo("  " + tr("!append, keep whole gop -> fileIndex %1").arg(fileIndex));
       name = Globals::getFileName(m_currentInput) + "_cut_" + numberToLength3String(fileIndex)
           + ".mkv";
       trim = "KEEP";
@@ -602,17 +605,32 @@ void MkvCutter::buildTrimAndPartsList()
     }
     if (prevKey >= nextKey) {
       nextKey = m_frameCount;
-      this->addInfo("  " + tr("prevKey >= nextKey -> nextKey = %1").arg(m_frameCount));
+      this->addInfo("  " + tr("prevKey >= nextKey"));
+      this->addInfo("   " + tr("nextKey = m_frameCount(%1)").arg(m_frameCount));
     }
+    bool prevKey_LastStartKey = prevKey == lastStartKey;
+    this->addInfo("  " + tr("prevKey == lastStartKey: %1").arg(prevKey_LastStartKey));
+    bool prevKey_LastNextKey = prevKey <= lastNextKey;
+    this->addInfo("  " + tr("prevKey <= lastNextKey: %1").arg(prevKey_LastNextKey));
     if (prevKey == lastStartKey) {
+      this->addInfo("  " + tr("prevKey == lastStartKey"));
+      this->addInfo("   " + tr("append = true"));
+      this->addInfo("   " + tr("fileEndKey && lastNextKey = nextKey(%1)").arg(nextKey));
       append = true;
       fileEndKey = nextKey;
       lastNextKey = nextKey;
-    } else if (prevKey == lastNextKey) {
+    } else if (prevKey <= lastNextKey) {
+      this->addInfo("  " + tr("prevKey <= lastNextKey"));
+      this->addInfo("   " + tr("append = true"));
+      this->addInfo("   " + tr("prevKey = lastStartKey(%1)").arg(lastStartKey));
+      this->addInfo("   " + tr("fileEndKey = nextKey(%1)").arg(nextKey));
+      if (nextKey > lastNextKey) {
+        this->addInfo("   " + tr(" nextKey > lastNextKey -> lastNextKey = nextKey(%1)").arg(nextKey));
+        lastNextKey = nextKey;
+      }
       append = true;
       fileEndKey = nextKey;
       prevKey = lastStartKey;
-      lastStartKey = prevKey;
       negReplace = QString::number(prevKey);
     } else {
       append = false;
